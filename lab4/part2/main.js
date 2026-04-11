e31705b2f73b955bb55bcfd88c88f6173dc8c908
StarOfEarendil <kyle.donnelly@dcmail.ca>
2026-04-10 21:50:34 -0400

    Update main.js

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -4,50 +4,56 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
-// Create a data object
+// 1. Create a data object
 const images = [
   { filename: "pic1.jpg", alt: "Closeup of a human eye" },
   { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
   { filename: "pic3.jpg", alt: "Purple and white pansies" },
   { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
-  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" }
 ];
 
-// Loop through the images using a for...of loop
+// 2. Add the images to the thumbnail bar
+
+// Create a baseURL constant (Set to empty string since images are locally saved)
+const baseURL = "";
+
+// Create a for...of loop
 for (const image of images) {
-  // Create a new image element
+  // Create a new <img> element
   const newImage = document.createElement("img");
   
-  // Set the source directly to the local file name and add alt text
-  newImage.src = image.filename;
+  // Set the src to a combination of baseURL and filename, and set alt text
+  newImage.src = baseURL + image.filename;
   newImage.alt = image.alt;
   
-  // Append the image as a child of the thumbBar
+  // Add attribute to make it focusable via keyboard
+  newImage.setAttribute("tabindex", "0");
+  
+  // Append the <img> to the thumbBar
   thumbBar.appendChild(newImage);
   
-  // Update the display to show the image full size when a thumb is clicked
+  // Add a click event handler
   newImage.addEventListener("click", updateDisplayedImage);
 }
 
-// Create the updateDisplayedImage() function
+// 3. Create the updateDisplayedImage() function
 function updateDisplayedImage(e) {
   displayedImage.src = e.target.src;
   displayedImage.alt = e.target.alt;
 }
 
-// Wire up the Darken/Lighten button
-btn.addEventListener("click", () => {
-  // If the button has a "dark" class set,
-  // change text to "Lighten" and make the overlay darker
+// 4. Wire up the Darken/Lighten button
+btn.addEventListener("click", function() {
+  // Conditional structure testing the class
   if (btn.classList.contains("dark")) {
     btn.textContent = "Lighten";
     overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
   } else {
-    // Else, change text to "Darken" and make
-    // the overlay lighter
     btn.textContent = "Darken";
     overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
   }
-  // Toggle the class ready for the next button press
+  
+  // Stretch goal: Toggle the dark class using a single line of code
   btn.classList.toggle("dark");
 });
