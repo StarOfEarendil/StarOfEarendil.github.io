3b499a884bcb76df3b63ee477898945cd5fcda96
StarOfEarendil <kyle.donnelly@dcmail.ca>
2026-04-10 22:12:15 -0400

    Update main.js
    updating the balls data

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -39,3 +39,27 @@ testBall.x;
 testBall.size;
 testBall.color;
 testBall.draw();
+
+class Ball {
+  // …
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
